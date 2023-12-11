a37634391a34996dbd1b851890ea11fb5c44fc79
Ryan Lee <ryanjlee98@gmail.com>
2023-12-11 14:59:11 -0800

    Defines winning conditions

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -143,11 +143,24 @@ public class TicTacToe implements ActionListener{
     }
 
     public void xWins(int a, int b, int c) {
+        buttons[a].setBackground(Color.GREEN);
+        buttons[b].setBackground(Color.GREEN);
+        buttons[c].setBackground(Color.GREEN);
 
+        for(int i = 0; i < 9; i++) {
+            buttons[i].setEnabled(false);
+        }
+        textfield.setText("X wins");
     }
 
     public void oWins(int a, int b, int c) {
+        buttons[a].setBackground(Color.GREEN);
+        buttons[b].setBackground(Color.GREEN);
+        buttons[c].setBackground(Color.GREEN);
 
+        for(int i = 0; i < 9; i++) {
+            buttons[i].setEnabled(false);
+        }
+        textfield.setText("O wins");
     }
-
 }
