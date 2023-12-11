a57cdfa9b7778c6f34fcee54347b9552412f22d4
Ryan Lee <ryanjlee98@gmail.com>
2023-12-11 13:51:31 -0800

    Adds frame and textfield constructors

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,10 +2,30 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
+
 public class TicTacToe implements ActionListener{
 
-    TicTacToe() {
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel title_panel = new JPanel();
+    JPanel button_panel = new JPanel();
+    JLabel textfield = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean player1_turn;
 
+    TicTacToe() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,800);
+        frame.getContentPane().setBackground(new Color(50,50,50));
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+
+        textfield.setBackground(new Color(25,25,25));
+        textfield.setForeground(new Color(25,255,0));
+        textfield.setFont(new Font("Ink Free",Font.BOLD,75));
+        textfield.setHorizontalAlignment(JLabel.CENTER);
+        textfield.setText("Tic-Tac-Toe");
+        textfield.setOpaque(true);
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -27,5 +47,5 @@ public class TicTacToe implements ActionListener{
     public void oWins(int a, int b, int c) {
 
     }
-    
+
 }
