d5fe7c1397ebf960a9e4f352727666d37aebecfc
Ryan Lee <ryanjlee98@gmail.com>
2023-12-11 12:22:51 -0800

    Adds methods

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,2 +1,31 @@
-public class TicTacToe {
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+public class TicTacToe implements ActionListener{
+
+    TicTacToe() {
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void firstTurn() {
+
+    }
+
+    public void check() {
+
+    }
+
+    public void xWins(int a, int b, int c) {
+
+    }
+
+    public void oWins(int a, int b, int c) {
+
+    }
+    
 }
