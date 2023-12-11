7029fe6f4aa276edc78780dc1a0d50d112c29bf1
Ryan Lee <ryanjlee98@gmail.com>
2023-12-11 14:30:01 -0800

    Defines firstTurn method and edits constructor

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -4,7 +4,6 @@ import java.util.*;
 import javax.swing.*;
 
 public class TicTacToe implements ActionListener{
-
     Random random = new Random();
     JFrame frame = new JFrame();
     JPanel title_panel = new JPanel();
@@ -26,6 +25,26 @@ public class TicTacToe implements ActionListener{
         textfield.setHorizontalAlignment(JLabel.CENTER);
         textfield.setText("Tic-Tac-Toe");
         textfield.setOpaque(true);
+
+        title_panel.setLayout(new BorderLayout());
+        title_panel.setBounds(0,0,800,100);
+
+        button_panel.setLayout(new GridLayout(3,3));
+        button_panel.setBackground(new Color(150,150,150));
+
+        for (int i = 0; i < 9; i++) {
+            buttons[i] = new JButton();
+            button_panel.add(buttons[i]);
+            buttons[i].setFont(new Font("MV Boli",Font.BOLD,120));
+            buttons[i].setFocusable(false);
+            buttons[i].addActionListener(this);
+        }
+
+        title_panel.add(textfield);
+        frame.add(title_panel,BorderLayout.NORTH);
+        frame.add(button_panel);
+
+        firstTurn();
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -33,7 +52,14 @@ public class TicTacToe implements ActionListener{
     }
 
     public void firstTurn() {
-
+        if(random.nextInt(2) == 0) {
+            player1_turn = true;
+            textfield.setText("X turn");
+        }
+        else {
+            player1_turn = false;
+            textfield.setText("O turn");
+        }
     }
 
     public void check() {
